c6c3a22de6f89566659ce1a1bf6b0570b6299329
Henry T <tran_ht@hotmail.com>
2017-04-06 17:14:45 +0000

    callbackArg.js can output indices. callbackArgForEach.js initial commit

---
+++ callbackArgForEach.js
@@ -0,0 +1,15 @@
+//test.js
+
+function findWaldo(arr, found) {
+  arr.foreach(function(arr))
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
