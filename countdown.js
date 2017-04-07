2a752523f7daeb930abd7679fdd8565f6b9a8c8c
Henry T <tran_ht@hotmail.com>
2017-04-07 13:07:51 +0000

    Countdown works, but the blast off needs to be implemented

--- countdown.js
+++ countdown.js
@@ -1,11 +1,19 @@
 var countdownGenerator = function (x) {
   /* your code here */
-};
+  var index = x
+    return function() {
+    /* your code here */
+    index = index - 1
+    return index + 1
+  }
+}
+
 
 var countdown = countdownGenerator(3);
-countdown(); // T-minus 3...
-countdown(); // T-minus 2...
-countdown(); // T-minus 1...
-countdown(); // Blast Off!
+
+console.log(countdown()); // T-minus 3...
+console.log(countdown()); // T-minus 2...
+console.log(countdown()); // T-minus 1...
+console.log(countdown()); // Blast Off!
 countdown(); // Rockets already gone, bub!
 countdown(); // Rockets already gone, bub!
